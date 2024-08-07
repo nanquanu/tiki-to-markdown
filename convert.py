e49f26609aa2b634fedf5357a4f5ddc2a292dffa
DrOteonu <oteonu@nanquanu.org>
2024-08-07 18:13:08 +0200

    Added parsing numbered lists and improved efficiency with elif constructs.

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import os
 
 file_name = sys.argv[1]
 
@@ -28,27 +29,26 @@ for i in range(lines_count):
         if '|' not in match.group(0):
             lines[i] = lines[i][:match.start(1)] + lines[i][match.end(1):match.start(2)] + lines[i][match.end(2):]
         match = search_basic_link()
-            
 
-    match = re.match('^!+', lines[i])
-    if match is not None:
+    if (match := re.match('^!+', lines[i])) is not None:
         lines[i] = "#" * (match.end() - match.start()) + " " + lines[i][match.end():]
-
-    match = re.match('^\++', lines[i])
-    if match is not None:
+    elif (match := re.match('^\++', lines[i])) is not None:
         lines[i] = "  " * (match.end() - match.start() - 1) + (" " if lines[i][match.end()] != " " else "") + " " + lines[i][match.end():]
-
-    match = re.match('^\*+', lines[i])
-    if match is not None:
+    elif (match := re.match('^\*+', lines[i])) is not None:
         lines[i] = "  " * (match.end() - match.start() - 1) + "-" + (" " if lines[i][match.end()] != " " else "") + lines[i][match.end():]
+    elif (match := re.match('^\#+', lines[i])) is not None: # numbered list 
+        lines[i] = "  " * (match.end() - match.start() - 1) + "1." + (" " if lines[i][match.end()] != " " else "") + lines[i][match.end():]
+
 
 for i in range(lines_count):
     lines[i] = lines[i].replace('[[', '[').replace("__", "**").replace("{CODE}", "\n```").replace("{CODE()}", "```\n")
     # TODO: If a code block starts with indentation, the indentation should be added for every line of the code block in Markdown.
     # TODO: Make sure closing code blocks always happens on a newline
         
-
-new_file_name = file_name.replace('.tiki', '.md')
+if os.path.splitext(file_name)[1].lower()[1:] == "tiki":
+    new_file_name = file_name.replace('.tiki', '.md')
+else:
+    new_file_name = file_name + '.md'
 
 with open(new_file_name, 'w') as file:
     file.writelines(lines)
